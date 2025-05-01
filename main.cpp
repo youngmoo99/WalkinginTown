330f046b8d0753d19b9ba4070ac1bbafee0aebfe
YoungMoo Kim <rladuan612@gmail.com>
2025-05-01 21:48:20 +0900

    update walk in town

--- main.cpp
+++ main.cpp
@@ -1,14 +1,31 @@
 #define SDL_MAIN_HANDLED
 #include <SDL.h>
 #include <stdio.h>
-
+#include <filesystem>
 #pragma comment(lib, "SDL2.lib")
 
 const int WINDOW_WIDTH = 640;
 const int WINDOW_HEIGHT = 480;
 const int TILE_SIZE = 32;
 const int SPEED = 5;
-
+const int MAP_WIDTH = 20;
+const int MAP_HEIGHT = 15;
+int tileMap[MAP_HEIGHT][MAP_WIDTH] = {
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,1,1,1,0,0,0,0,0,1,1,1,0,0,0,0,0,1,1,0},
+    {0,1,0,1,0,0,0,0,0,1,0,1,0,0,0,0,0,1,0,0},
+    {0,1,0,1,0,0,0,0,0,1,0,1,0,0,0,0,0,1,0,0},
+    {0,1,0,1,0,0,0,0,0,1,0,1,0,0,0,0,0,1,0,0},
+    {0,1,0,1,0,0,0,0,0,1,0,1,0,0,0,0,0,1,0,0},
+    {0,1,0,1,0,0,0,0,0,1,0,1,0,0,0,0,0,1,0,0},
+    {0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0},
+    {0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0},
+    {0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+};
 SDL_Window* window = nullptr;
 SDL_Renderer* renderer = nullptr;
 SDL_Texture* mapTexture = nullptr;
@@ -20,6 +37,8 @@ void HandleInput(bool& isRunning);
 void Update();
 void Render();
 void Cleanup();
+void RenderMap(SDL_Texture* tileTexture);
+void TryMovePlayer(int dx, int dy);
 
 int main(int argc, char* argv[])
 {
@@ -59,7 +78,7 @@ bool Init() {
     }
 
     // 맵 타일 이미지 불러오기 (나중에)
-    SDL_Surface* mapSurface = SDL_LoadBMP("assets/map.bmp");
+    SDL_Surface* mapSurface = SDL_LoadBMP("assets/map_tiles.bmp");
     mapTexture = SDL_CreateTextureFromSurface(renderer, mapSurface);
     SDL_FreeSurface(mapSurface);
 
@@ -67,6 +86,7 @@ bool Init() {
     SDL_Surface* playerSurface = SDL_LoadBMP("assets/player.bmp");
     playerTexture = SDL_CreateTextureFromSurface(renderer, playerSurface);
     SDL_FreeSurface(playerSurface);
+
 
     return true;
 }
@@ -79,20 +99,20 @@ void HandleInput(bool& isRunning) {
 
     const Uint8* keys = SDL_GetKeyboardState(NULL);
 
-    if (keys[SDL_SCANCODE_LEFT])  playerRect.x -= SPEED;
-    if (keys[SDL_SCANCODE_RIGHT]) playerRect.x += SPEED;
-    if (keys[SDL_SCANCODE_UP])    playerRect.y -= SPEED;
-    if (keys[SDL_SCANCODE_DOWN])  playerRect.y += SPEED;
+    if (keys[SDL_SCANCODE_LEFT])  TryMovePlayer(-SPEED, 0);
+    if (keys[SDL_SCANCODE_RIGHT]) TryMovePlayer(SPEED, 0);
+    if (keys[SDL_SCANCODE_UP])    TryMovePlayer(0, -SPEED);
+    if (keys[SDL_SCANCODE_DOWN])  TryMovePlayer(0, SPEED);
 }
 
 void Render() {
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // 배경 검정
     SDL_RenderClear(renderer);
 
-    // 맵 그리기
-    SDL_RenderCopy(renderer, mapTexture, NULL, NULL);
+    //타일맵 랜더링
+    RenderMap(mapTexture);
 
-    // 플레이어 그리기
+    // 플레이어랜더링
     SDL_RenderCopy(renderer, playerTexture, NULL, &playerRect);
 
     SDL_RenderPresent(renderer);
@@ -106,3 +126,40 @@ void Cleanup() {
     SDL_Quit();
 }
 
+//맵 렌더링
+void RenderMap(SDL_Texture* tileTexture) {
+    SDL_Rect src, dest;
+    src.w = dest.w = TILE_SIZE;
+    src.h = dest.h = TILE_SIZE;
+
+    for (int y = 0; y < MAP_HEIGHT; y++) {
+        for (int x = 0; x < MAP_WIDTH; x++) {
+            int tile = tileMap[y][x];
+            src.x = tile * TILE_SIZE;
+            src.y = 0;
+            dest.x = x * TILE_SIZE;
+            dest.y = y * TILE_SIZE;
+
+            SDL_RenderCopy(renderer, tileTexture, &src, &dest);
+        }
+    }
+}
+
+// 충돌 검사 후 이동 허용 
+void TryMovePlayer(int dx, int dy) {
+    int nextX = playerRect.x + dx;
+    int nextY = playerRect.y + dy;
+
+    int tileX = nextX / TILE_SIZE;
+    int tileY = nextY / TILE_SIZE;
+
+    if (tileMap[tileY][tileX] == 0) {
+        playerRect.x = nextX;
+        playerRect.y = nextY;
+    }
+}
+
+
+void Update() {
+   
+}
